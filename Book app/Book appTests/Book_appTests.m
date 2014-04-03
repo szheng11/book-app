9c117459aa898cbedf9864853a507e77e964adef
szheng11 <shuzheng.zheng@globalonlineacademy.org>
2014-04-03 00:53:47 -0500

    initial commit
    Setting up Github

---
+++ Book app/Book appTests/Book_appTests.m
@@ -0,0 +1,34 @@
+//
+//  Book_appTests.m
+//  Book appTests
+//
+//  Created by Shuzheng Zheng on 4/3/14.
+//  Copyright (c) 2014 Shuzheng Zheng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Book_appTests : XCTestCase
+
+@end
+
+@implementation Book_appTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
